f15103324edc9db775495bb9ca61eaac55d0497f
YaroslavBry <riky.tiky@gmail.com>
2022-07-12 22:37:47 +0300

    12 07 22

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector(".mob-menu"),
+    closeMenuBtn: document.querySelector(".mob-menu-close"),
+    menu: document.querySelector(".mobile-menu"),
+    body: document.querySelector("body"),
+  };
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
